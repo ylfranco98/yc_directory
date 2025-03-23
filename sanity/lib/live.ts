bfd423e0a5d8183d46ab4c14172e923ff76c88f4
Yoana <ylfranco98@gmail.com>
2025-03-23 18:58:30 -0400

    first commit

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -5,6 +5,6 @@ import { client } from "@/sanity/lib/client";
 
 export const { sanityFetch, SanityLive } = defineLive({
   client,
-  serverToken: process.env.SANITY_SERVER_TOKEN, // Use a token with more privileges (server-side)
-  browserToken: process.env.SANITY_BROWSER_TOKEN, // Use a read-only token (exposed to the browser)
+  // serverToken: process.env.SANITY_SERVER_TOKEN, // Use a token with more privileges (server-side)
+  // browserToken: process.env.SANITY_BROWSER_TOKEN, // Use a read-only token (exposed to the browser)
 });
