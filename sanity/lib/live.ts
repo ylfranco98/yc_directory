503cdaa604cdbab7fef41891261fbefa2d6dd087
Yoana <ylfranco98@gmail.com>
2025-03-29 12:48:47 -0400

    first commit

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -3,4 +3,11 @@ import "server-only";
 import { defineLive } from "next-sanity";
 import { client } from "@/sanity/lib/client";
 
-export const { sanityFetch, SanityLive } = defineLive({ client });
+export const { sanityFetch, SanityLive } = defineLive({
+  client,
+  // Add serverToken to allow secure fetching of drafts on the server
+  serverToken: process.env.SANITY_SERVER_TOKEN,
+
+  // Add browserToken to enable draft previews in the browser
+  browserToken: process.env.SANITY_BROWSER_TOKEN,
+});
